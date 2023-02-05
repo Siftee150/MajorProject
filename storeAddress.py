42c2d6ad53af83ec4d157f8970073657f4831ba3
Siftee15 <sifs.ratra5677@gmail.com>
2023-02-06 01:43:34 +0530

    phishingaddresses compile

--- storeAddress.py
+++ storeAddress.py
@@ -1,10 +1,21 @@
 import pickle
-
-# open a file, where you stored the pickled data
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Firefox()
 file = open('./file.pkl', 'rb')
-
-# dump information to that file
-data = pickle.load(file)
-print(len(data))
-# close the file
+url_data = pickle.load(file)
+print("Parsing total urls "+str(len(url_data)))
+phishing_addresses = []
+for url in url_data:
+    addressXpath = "/html/body/div/div/div/div[3]/div[1]/div[1]/div[8]/ul/li/a"
+    try:
+        driver.get(url=url)
+        address = driver.find_element(By.XPATH, addressXpath)
+        phishing_addresses.append([address.text])
+    except:
+        continue
+with open('phishingaddress', 'w') as f:
+    write = csv.writer(f)
+    write.writerows(phishing_addresses)
 file.close()
