f09c8d9d18b6e817874c50e1c8b86ebabe747d1f
Siftee15 <sifs.ratra5677@gmail.com>
2023-03-24 14:49:54 +0530

    Sample graph visualiser

--- graphCreator.py
+++ graphCreator.py
@@ -2,6 +2,8 @@ import pandas as pd
 import networkx as nx
 import os
 import re
+import matplotlib.pyplot as plt
+from node2vec import Node2Vec
 PATH = './Dataset'
 
 
@@ -29,3 +31,15 @@ def traverseDataset(path, depth):
 G = nx.DiGraph()
 traverseDataset(PATH, 0)
 print(G)
+
+# saving graph created above in gexf format
+nx.write_gexf(G, "graph_visual.gexf")
+
+
+# # Precompute probabilities and generate walks - **ON WINDOWS ONLY WORKS WITH workers=1**
+# node2vec = Node2Vec(G, dimensions=64, walk_length=30,
+#                     num_walks=200, workers=4)  # Use temp_folder for big graphs
+
+# # Embed nodes
+# # Any keywords acceptable by gensim.Word2Vec can be passed, `dimensions` and `workers` are automatically passed (from the Node2Vec constructor)
+# model = node2vec.fit(window=10, min_count=1, batch_words=4)
